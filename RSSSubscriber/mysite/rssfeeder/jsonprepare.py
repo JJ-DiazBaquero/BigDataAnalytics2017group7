ff9ff76d31db48bb0dd4d816a437f33ea06f191c
Juan Camilo Ortiz R <juanco.ortizr@gmail.com>
2017-04-05 14:44:46 -0500

    asd

--- RSSSubscriber/mysite/rssfeeder/jsonprepare.py
+++ RSSSubscriber/mysite/rssfeeder/jsonprepare.py
@@ -9,8 +9,8 @@ def line_prepender(filename, line):
 os.remove("/home/estudiante/BigDataT2/BigDataAnalytics2017group7/RSSSubscriber/mysite/rssfeeder/static/pruebavis/nodes.js")
 os.remove("/home/estudiante/BigDataT2/BigDataAnalytics2017group7/RSSSubscriber/mysite/rssfeeder/static/pruebavis/edges.js")
 
-line_prepender("/home/estudiante/BigDataT2/BigDataAnalytics2017group7/RSSSubscriber/mysite/rssfeeder/static/pruebavis/n1184/part-00000", "var nodes:[")
-line_prepender("/home/estudiante/BigDataT2/BigDataAnalytics2017group7/RSSSubscriber/mysite/rssfeeder/static/pruebavis/r1184/part-00000", "var edges:[")
+line_prepender("/home/estudiante/BigDataT2/BigDataAnalytics2017group7/RSSSubscriber/mysite/rssfeeder/static/pruebavis/n1182/part-00000", "var nodes:[")
+line_prepender("/home/estudiante/BigDataT2/BigDataAnalytics2017group7/RSSSubscriber/mysite/rssfeeder/static/pruebavis/r1182/part-00000", "var edges:[")
 
 myfile = open("/home/estudiante/BigDataT2/BigDataAnalytics2017group7/RSSSubscriber/mysite/rssfeeder/static/pruebavis/n1184/part-00000", "a")
 myfile.write("]")
