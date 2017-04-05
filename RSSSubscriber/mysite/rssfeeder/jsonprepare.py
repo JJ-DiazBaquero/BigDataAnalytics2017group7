a4b396a2f1ee9a669bead5d72c9c6e67765b57ed
Juan Camilo Ortiz R <juanco.ortizr@gmail.com>
2017-04-05 14:42:13 -0500

    asd

--- RSSSubscriber/mysite/rssfeeder/jsonprepare.py
+++ RSSSubscriber/mysite/rssfeeder/jsonprepare.py
@@ -1,3 +1,4 @@
+import os
 def line_prepender(filename, line):
     with open(filename, 'r+') as f:
         content = f.read()
@@ -5,12 +6,17 @@ def line_prepender(filename, line):
         f.write(line.rstrip('\r\n') + '\n' + content)
     return f
 
+os.remove("/home/estudiante/BigDataT2/BigDataAnalytics2017group7/RSSSubscriber/mysite/rssfeeder/static/pruebavis/nodes.js")
+os.remove("/home/estudiante/BigDataT2/BigDataAnalytics2017group7/RSSSubscriber/mysite/rssfeeder/static/pruebavis/edges.js")
 
 line_prepender("/home/estudiante/BigDataT2/BigDataAnalytics2017group7/RSSSubscriber/mysite/rssfeeder/static/pruebavis/n1184/part-00000", "var nodes:[")
 line_prepender("/home/estudiante/BigDataT2/BigDataAnalytics2017group7/RSSSubscriber/mysite/rssfeeder/static/pruebavis/r1184/part-00000", "var edges:[")
 
 myfile = open("/home/estudiante/BigDataT2/BigDataAnalytics2017group7/RSSSubscriber/mysite/rssfeeder/static/pruebavis/n1184/part-00000", "a")
 myfile.write("]")
+os.rename("/home/estudiante/BigDataT2/BigDataAnalytics2017group7/RSSSubscriber/mysite/rssfeeder/static/pruebavis/n1184/part-00000", "/home/estudiante/BigDataT2/BigDataAnalytics2017group7/RSSSubscriber/mysite/rssfeeder/static/pruebavis/nodes.js")
+
+os.rename("/home/estudiante/BigDataT2/BigDataAnalytics2017group7/RSSSubscriber/mysite/rssfeeder/static/pruebavis/r1184/part-00000", "/home/estudiante/BigDataT2/BigDataAnalytics2017group7/RSSSubscriber/mysite/rssfeeder/static/pruebavis/edges.js")
 myfile.close()
 
 myfile2 = open("/home/estudiante/BigDataT2/BigDataAnalytics2017group7/RSSSubscriber/mysite/rssfeeder/static/pruebavis/r1184/part-00000", "a")
